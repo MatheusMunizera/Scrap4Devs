6319848d2df7f3a093a5fe884569cc121bb3653a
MatheusMunizera <matheus.munizera@gmail.com>
2022-12-03 15:37:02 -0300

    teste title goolge

--- src/scrapper/scrapper.service.ts
+++ src/scrapper/scrapper.service.ts
@@ -26,33 +26,43 @@ export class ScrapperService {
             };
         }
 
-        const URL = `https://www.4devs.com.br/gerador_de_pessoas`;
-
         try {
-
-            const browser = await puppeteer.launch(options);
-            const page = await browser.newPage();
-            await page.goto(URL, { "waitUntil": "networkidle2" });
-            await page.evaluate(async () => {
-                document.querySelector('#bt_gerar_pessoa').dispatchEvent(new CustomEvent('click'));
-            });
-
-            await page.waitForFunction(
-                'document.querySelector("#dados_json").innerText.includes("nome")'
-            );
-
-            const results = await page.evaluate(async () => {
-                const data = document.querySelector('#dados_json').textContent;
-                return data;
-            });
-
-
-            console.log('getDataViaPuppeteer results :', results);
-            return JSON.parse(results);
-        } catch (err) {
+            let browser = await puppeteer.launch(options);
+        
+            let page = await browser.newPage();
+            await page.goto("https://www.google.com");
+            return await page.title();
+          } catch (err) {
             console.error(err);
             return null;
-        }
+          }
+        }; 
 
-    }
+        // try {
+        //     const URL = `https://www.4devs.com.br/gerador_de_pessoas`;
+        //     const browser = await puppeteer.launch(options);
+        //     const page = await browser.newPage();
+        //     await page.goto(URL, { "waitUntil": "networkidle2" });
+        //     await page.evaluate(async () => {
+        //         document.querySelector('#bt_gerar_pessoa').dispatchEvent(new CustomEvent('click'));
+        //     });
+
+        //     await page.waitForFunction(
+        //         'document.querySelector("#dados_json").innerText.includes("nome")'
+        //     );
+
+        //     const results = await page.evaluate(async () => {
+        //         const data = document.querySelector('#dados_json').textContent;
+        //         return data;
+        //     });
+
+
+        //     console.log('getDataViaPuppeteer results :', results);
+        //     return JSON.parse(results);
+        // } catch (err) {
+        //     console.error(err);
+        //     return null;
+        // }
+
+    
 }
