248d50dbd666893fcc8098c5c379a32aa201762b
MatheusMunizera <matheus.munizera@gmail.com>
2022-12-03 15:13:26 -0300

    ajustes

--- src/scrapper/scrapper.service.ts
+++ src/scrapper/scrapper.service.ts
@@ -1,39 +1,59 @@
 import { Injectable } from '@nestjs/common';
-import * as puppeteer from 'puppeteer-core';
-import chrome from 'chrome-aws-lambda';
+
 
 @Injectable()
 export class ScrapperService {
     async getDataViaPuppeteer() {
-        const URL = `https://www.4devs.com.br/gerador_de_pessoas`;
+        let chrome: any = {};
+        let puppeteer;
 
-        const options = {
-            args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+            chrome = require("chrome-aws-lambda");
+            puppeteer = require("puppeteer-core");
+        } else {
+            puppeteer = require("puppeteer");
         }
 
-        const browser = await puppeteer.launch(options);
-        const page = await browser.newPage();
-        await page.goto(URL, { "waitUntil": "networkidle2" });
-        await page.evaluate(async () => {
-            document.querySelector('#bt_gerar_pessoa').dispatchEvent(new CustomEvent('click'));
-        });
+        let options = {};
 
-        await page.waitForFunction(
-            'document.querySelector("#dados_json").innerText.includes("nome")'
-        );
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+            options = {
+                args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+                defaultViewport: chrome.defaultViewport,
+                executablePath: await chrome.executablePath,
+                headless: true,
+                ignoreHTTPSErrors: true,
+            };
+        }
 
-        const results = await page.evaluate(async () => {
-            const data = document.querySelector('#dados_json').textContent;
-            return data;
-        });
+        const URL = `https://www.4devs.com.br/gerador_de_pessoas`;
+
+        try {
+
+            const browser = await puppeteer.launch(options);
+            const page = await browser.newPage();
+            await page.goto(URL, { "waitUntil": "networkidle2" });
+            await page.evaluate(async () => {
+                document.querySelector('#bt_gerar_pessoa').dispatchEvent(new CustomEvent('click'));
+            });
+
+            await page.waitForFunction(
+                'document.querySelector("#dados_json").innerText.includes("nome")'
+            );
+
+            const results = await page.evaluate(async () => {
+                const data = document.querySelector('#dados_json').textContent;
+                return data;
+            });
 
 
-        console.log('getDataViaPuppeteer results :', results);
-        await browser.close();
-        return results;
+            console.log('getDataViaPuppeteer results :', results);
+            await browser.close();
+            return JSON.parse(results);
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+
     }
 }
